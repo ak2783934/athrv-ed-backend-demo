d1ef0979a4be988e47a40376103c66ea83334a5d
Avinash kumar <ak2783934@gmail.com>
2021-02-25 20:15:50 +0530

    add

---
+++ routes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+
+//postgres connection is here
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+//?post routes
+//! putting the name in the list in the front page {no auth required here}
+
+router.post("/registration", async (req, res) => {
+  try {
+    const { name, age, phone, email, college, eventno } = req.body;
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO resigtrations (name,age,phone,email,college,eventno) VALUES $1 $2 $3 $4 $5 $6 RETURNING *",
+      [name, age, phone, email, college, eventno]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//! posting a new event {auth required here}
+//! signup
+//! signin
+
+//?delete routes
+//! nowhere use at all
+
+//?put routes
+//! only to change the state of the event from active to inactive {auth is required here }
+
+//list all the routes you are going to work on!!
+//? get routes
+//! 1) getall the events {no auth required} //will be used both inside and outside
+
+//! 2) get all the persons list registered at certain event {auth required}
+//! 3)
+
+//try route
+
+app.get("/", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users");
+    const results = { results: result ? result.rows : null };
+    res.send(JSON.stringify(results));
+  } catch (err) {
+    console.log(err);
+  }
+});
