989fe755a1fe5277f5d517946c5993842f6d29ec
Avinash kumar <ak2783934@gmail.com>
2021-02-25 15:08:18 +0530

    another commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = requrie("cors");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
+//postgres connection is here
 const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -12,14 +13,12 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+const client = pool.connect();
 
 app.get("/db", async (req, res) => {
   try {
-    const client = await pool.connect();
     const result = await client.query("SELECT * FROM test_table");
-    const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
-    client.release();
+    res.json("we are connected ");
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
