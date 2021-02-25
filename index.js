ff0fc6348bfe6a62b3a1ebde5c32c113d1340002
Avinash kumar <ak2783934@gmail.com>
2021-02-25 14:01:31 +0530

    look for all

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = requrie("cors");
+
+app.use(express.json());
+app.use(cors());
+
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.connect(5000, () => {
+  console.log("app is running at 5000");
+});
---
+++ schemas.sql
@@ -0,0 +1,6 @@
+-- here we will be creating 3 tables 
+-- First is gonna be users
+-- second is registrations
+-- third is events 
+
+-- First step is to create a database for us
