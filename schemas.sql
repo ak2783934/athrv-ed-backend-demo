f6e345dacba92810cbb003e45af884e30ee4e55c
Avinash kumar <ak2783934@gmail.com>
2021-02-27 00:09:46 +0530

    schemas added

--- schemas.sql
+++ schemas.sql
@@ -3,9 +3,33 @@
 -- second is registrations
 -- third is events 
 
--- First step is to create a database for us
-heroku
-pg:
-psql
+-- Database used is the heroku postgress as a service
 
 
+CREATE TABLE users
+(
+    uid SERIAL NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    isadmin BOOLEAN DEFAULT FALSE,
+);
+
+CREATE TABLE registrations
+(
+    rid SERIAL NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    age INT NOT NULL,
+    phone VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    college VARCHAR(200) NOT NULL,
+    eventno INT NOT NULL,
+);
+
+CREATE TABLE event
+(
+    eid SERIAL NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    isactive BOOLEAN DEFAULT TRUE,
+);
+-- This is how 3 tables are created here. 
